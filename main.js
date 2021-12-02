5d60ad5eaeccbe4b141582d2b47c71dee73cf284
sandranielsen <eaasmmn@students.eaaa.com>
2021-12-02 15:02:53 +0100

    adding pages + css

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ import ChatPage from "./pages/chat.js";
 import AddListingPage from "./pages/add-listing.js";
 import FavouritesPage from "./pages/favourites.js";
 import ProfilePage from "./pages/profile.js";
+import AccountPage from "./pages/account.js";
+import FilterPage from "./pages/filter.js";
+import LogInPage from "./pages/login.js";
+import MyImpactPage from "./pages/my-impact.js";
+import MyListingsPage from "./pages/my-listings.js";
+import MyPurchasesPage from "./pages/my-purchases.js";
+import ProductPage from "./pages/product-page.js";
+import PublicProfilePage from "./pages/public-profile.js";
+import SignUpPage from "./pages/signup.js";
+import StartPage from "./pages/start.js";
 import spaService from "./services/spa.js";
 
 // Declare and init
@@ -13,6 +23,16 @@ let chatPage = new ChatPage();
 let addListingPage = new AddListingPage();
 let favouritesPage = new FavouritesPage();
 let profilePage = new ProfilePage();
+let accountPage = new AccountPage();
+let filterPage = new FilterPage();
+let logInPage = new LogInPage();
+let myImpactPage = new MyImpactPage();
+let myListingsPage = new MyListingsPage();
+let myPurchasesPage = new MyPurchasesPage();
+let productPage = new ProductPage();
+let publicProfilePage = new PublicProfilePage();
+let signUpPage = new SignUpPage();
+let startPage = new StartPage();
 
 // Init services
 spaService.init();
---
+++ pages/product-page.js
@@ -0,0 +1,17 @@
+export default class ProductPage {
+  constructor() {
+    this.template();
+  }
+
+  template() {
+    document.querySelector("#app").innerHTML += /*html*/ `
+      <section id="product" class="page">
+        <header class="topbar">
+          <h2>About</h2>
+        </header>
+        <h3>About me</h3>
+        <p>Some info about me. Enjoy!</p>
+      </section>
+    `;
+  }
+}
