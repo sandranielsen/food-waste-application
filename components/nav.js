1a0a3eb16251d5cdc30b88aa3535ef5f8ad95384
sandranielsen <eaasmmn@students.eaaa.com>
2021-12-03 13:53:14 +0100

    nav almost done

--- components/nav.js
+++ components/nav.js
@@ -6,11 +6,17 @@ export default class NavBar {
   template() {
     document.querySelector("#app").innerHTML += /*html*/ `
       <nav class="tabbar">
-        <a href="#home">Home</a>
-        <a href="#chat">Chat</a>
-        <a href="#add">Add Listing</a>
-        <a href="#favourites">Favourites</a>
-        <a href="#profile">Profile</a>
+        <div class="nav_left">
+          <a href="#home" class="nav_item"><img src="media/home.svg" alt="home" class="nav_img"></a>
+          <a href="#chat" class="nav_item"><img src="media/chat.svg" alt="chat" class="nav_img"></a>
+        </div>
+        <div class="nav_middle">
+          <a href="#add" class="nav_item_raised"><img src="media/add.svg" alt="plus" class="nav_img_raised"></a>
+        </div>
+        <div class="nav_right">
+          <a href="#favourites" class="nav_item"><img src="media/fav.svg" alt="heart" class="nav_img"></a>
+          <a href="#profile" class="nav_item"><img src="media/profile.svg" alt="profile" class="nav_img"></a>
+        </div>
       </nav>
     `;
   }
