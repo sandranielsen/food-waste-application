8d981f02d4f821c84fca557fd19eb2da0b569d89
Sona Juhasova <soj@web2media.dk>
2021-12-06 01:53:12 +0100

    pages filled in

--- pages/my-impact.js
+++ pages/my-impact.js
@@ -9,8 +9,26 @@ export default class MyImpactPage {
         <header class="topbar">
           <h2>About</h2>
         </header>
-        <h3>About me</h3>
-        <p>Some info about me. Enjoy!</p>
+
+        <h2>My Impact Tracker</h2>
+            <div class="impact-tracker-wrapper">
+                <div class="impact-tracker">
+                  <h3>3</h3>
+                  <h4>Purchase(s)<br>made</h4>
+                </div>
+                <div class="impact-tracker">
+                  <h3>3</h3>
+                  <h4>Listing(s)<br>made</h4>
+                </div>
+            </div>
+
+            <h2>Badges</h2>
+            <div class="impact-badges-wrapper">
+                <div class="impact-badge">
+                    
+                
+                </div>
+            </div>
       </section>
     `;
   }
--- pages/my-purchases.js
+++ pages/my-purchases.js
@@ -5,12 +5,43 @@ export default class MyPurchasesPage {
 
   template() {
     document.querySelector("#app").innerHTML += /*html*/ `
-      <section id="purchases" class="page">
-        <header class="topbar">
-          <h2>About</h2>
-        </header>
-        <h3>About me</h3>
-        <p>Some info about me. Enjoy!</p>
+    <header class="topbar">
+       <h2>My Purchases</h2>
+    </header>
+     <section id="purchases" class="page">
+       
+
+
+            <!-- Product listings -->
+            <div class="product-listing-container">
+            <div class="product-listing-image">
+                <button class="favourite-button">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="14.19" height="13.799" viewBox="0 0 14.19 13.799">
+                  <path id="Icon_ionic-ios-heart" data-name="Icon ionic-ios-heart" d="M13.014,3.938h-.032A3.608,3.608,0,0,0,9.97,5.586,3.608,3.608,0,0,0,6.958,3.938H6.926A3.585,3.585,0,0,0,3.375,7.52a7.718,7.718,0,0,0,1.516,4.208,26.561,26.561,0,0,0,5.08,4.893,26.561,26.561,0,0,0,5.08-4.893A7.718,7.718,0,0,0,16.565,7.52,3.585,3.585,0,0,0,13.014,3.938Z" transform="translate(-2.875 -3.438)" fill="none" stroke="#13553f" stroke-width="1"/>
+                  </svg>
+                </button>        
+            </div>
+            <div class="product-listing-info-container">
+              <h2>Whole grain noodles</h2>
+              <div style="margin-top: 25px;">
+                  <p style="font-weight:400; padding-right: 5px;">Expiration date:</p>
+                  <p>07.06.2023</p>
+              </div>
+
+              <div class="product-listing-user-info">
+                  <div>
+                    <div class="product-listing-profile-img"></div>
+                    <p>Maria N.</p>
+                  </div> 
+                  <div>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="13.195" height="19.06" viewBox="0 0 13.195 19.06">
+                      <path id="Icon_ionic-ios-pin" data-name="Icon ionic-ios-pin" d="M14.473,3.375A6.377,6.377,0,0,0,7.875,9.5c0,4.765,6.6,12.934,6.6,12.934s6.6-8.169,6.6-12.934A6.377,6.377,0,0,0,14.473,3.375Zm0,8.747a2.149,2.149,0,1,1,2.149-2.149A2.149,2.149,0,0,1,14.473,12.122Z" transform="translate(-7.875 -3.375)" fill="#13553f"/>
+                      </svg>
+                      <p>Aarhus</p> 
+                  </div>
+              </div>
+            </div>
+        </div>
       </section>
     `;
   }
