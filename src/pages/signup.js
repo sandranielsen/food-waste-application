f1949516f87f062098af8e0037313ddec918c2e7
Sona Juhasova <soj@web2media.dk>
2021-12-08 19:15:26 +0100

    login and signup

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -4,7 +4,6 @@ import service from "../service.js";
 export default class SignUpPage {
   constructor(id) {
     this.id = id;
-    this.backImg = require("../img/back.svg");
     this.signupImg = require("../img/signup.jpg");
     this.render();
   }
@@ -19,27 +18,29 @@ export default class SignUpPage {
     var pc = document.querySelector("#signup-password-check").value;
     debugger;
     console.log(n);
-    var response = service.signupUser(n,u,p,pc);
+    var response = service.signupUser(n,u,p,pc).then((data)=>{
+      debugger;
+      if (data.signupSuccess) {
+        document.querySelector(".signup-message").innerHTML = "";
+        router.navigateTo("/login");
+      } else {
+        document.querySelector(".signup-message").innerHTML = data.error;
+      }
+    });
 
-    if (response.signupSuccess) {
-      document.querySelector(".signup-message").innerHTML = "";
-      navigateTo("/login");
-    } else {
-      document.querySelector(".signup-message").innerHTML = data.error;
-    }
+
   }
   render() {
     document.querySelector("#root").insertAdjacentHTML(
       "beforeend",
-      /*html*/ `
+      /*jsx*/ `
       <section id="${this.id}" class="page">
-      <!--- Topbar container --->
         <header class="topbar">
-          <div class="topbar_img>">
-            <a href="/home"><img src="${this.backImg}"></a>
-          </div>
+          <a href="/home"><svg xmlns="http://www.w3.org/2000/svg" width="13.503" height="23.619" viewBox="0 0 13.503 23.619">
+          <path id="Icon_ionic-ios-arrow-back" data-name="Icon ionic-ios-arrow-back" d="M15.321,18l8.937-8.93a1.688,1.688,0,0,0-2.391-2.384L11.742,16.8a1.685,1.685,0,0,0-.049,2.327L21.86,29.32a1.688,1.688,0,0,0,2.391-2.384Z" transform="translate(-11.251 -6.194)" fill="#13553f"/>
+          </svg></a>
         </header>
-        <div class="banner_container">
+        <div class="login_signup_container">
           <div class="login_signup_img">
             <img src="${this.signupImg} alt="Food" class="login_signup_bg">
             <div class="half-overlay"></div>
@@ -49,18 +50,17 @@ export default class SignUpPage {
           </div>
         </div>
 
-        
         <div class="login_signup_container">
             <h1 class="login_signup_headline">Signup</h1>
             <div class="form_container">
             <form>
-              <input id="signup-name" class="signup_input" type="text" name="name" placeholder="Name">
-              <input id="signup-username" class="signup_input"  type="text" placeholder="Email" autocomplete="new-email">
-              <input id="signup-password" class="signup_input"  type="password" placeholder="Password" autocomplete="new-password">
-              <input id="signup-password-check" class="signup_input"  type="password" placeholder="Password" autocomplete="new-password">
+        <input id="signup-name" type="text" name="name" placeholder="Type Name">
+        <input id="signup-username" type="text" placeholder="Type email" autocomplete="new-email">
+        <input id="signup-password" type="password" placeholder="Password" autocomplete="new-password">
+        <input id="signup-password-check" type="password" placeholder="Password" autocomplete="new-password">
 
-        <button type="button" class="btn_alt" id="btn-signup">Sign up</button>
-        <div class=".signup-message"></div>
+        <button type="button" id="btn-signup">Sign up</button>
+        <div class="signup-message"></div>
       </form>
 
             </div>
--- src/service.js
+++ src/service.js
@@ -3,18 +3,17 @@ class Service {
     this.listings = [];
     this.baseUrl = "http://foodwaste.sonajuhasova.com/backend/";
     this.loginUrl = this.baseUrl + "/login.php";
-    this.selectedListingId;
   }
 
-  /***** Login/Signup *****/
-  /* Fetch and return all listings from backend service */
-  async signupUser(name, username, password, passwordCheck) {
-    const url = `${this.loginUrl}?action=signup`;
+  // rasmus' code
+  async signupUser(name,username,password,passwordCheck)
+  {
+    const url =`${this.loginUrl}?action=signup`;
     var data = {
-      name: name,
-      username: username,
-      password: password,
-      passwordCheck: passwordCheck,
+      'name':name,
+      'username':username,
+      'password':password,
+      'passwordCheck':passwordCheck
     };
     const response = await fetch(url, {
       method: "POST",
@@ -22,29 +21,43 @@ class Service {
     });
     const json = await response.json();
     return json;
+
   }
 
-  /************** CRUD operations **************/
-  
-  // Inspiration: Rasmus - parcel dating spa & user service
-  /***** Read listing *****/
+  async loginUser(name,password)
+  {
+    const url =`${this.loginUrl}?action=login`;
+    var data = {
+      'username':name,
+      'password':password,
+    };
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data), // parsing js object to json object
+    });
+    const json = await response.json();
+    return json;
+
+  }
+
   async getListings() {
     const url = `${this.baseUrl}?action=getListings`;
     const response = await fetch(url);
     const data = await response.json();
     this.listings = data;
     return this.listings;
-  }
+  } 
 
-  async getListing(listingId) {
-    const url = `${this.baseUrl}?action=getListing&listingId=${listingId}`;
+
+  async getUser(userId) {
+    const url = `${this.baseUrl}?action=getUser&userId=${userId}`;
     const response = await fetch(url);
-    const listing = await response.json();
-    return listing;
+    const user = await response.json();
+    return user;
   }
+  
 
-  /***** Create listing *****/
-  /* Image upload */
+
   async uploadImage(imageFile) {
     let formData = new FormData();
     formData.append("fileToUpload", imageFile);
@@ -61,22 +74,31 @@ class Service {
     return result;
   }
 
-  async createListing(
-    title,
-    price,
-    description,
-    expirationDate,
-    location,
-    image
-  ) {
+  
+
+  async deleteListing(listingId) {
+    const response = await fetch(
+      `${this.baseUrl}?action=deleteListing&listingId=${listingId}`,
+      {
+        method: "DELETE",
+      }
+    );
+    // waiting for the result
+    const result = await response.json();
+    // the result is the new updated listings array
+    this.listing = result;
+    return this.listing;
+  } 
+
+  async createListing(title, price, expirationDate, description, location, image) {
     const id = Date.now(); // dummy generated listing id
     const newListing = {
       // declaring a new js object with the form values
       id,
       title,
       price,
-      description,
       expirationDate,
+      description,
       location,
       image,
     };
@@ -93,40 +115,16 @@ class Service {
     return this.listing;
   }
 
-  /***** Delete listing */
-  async deleteListing(listingId) {
-    const response = await fetch(
-      `${this.baseUrl}?action=deleteListing&listingId=${listingId}`,
-      {
-        method: "DELETE",
-      }
-    );
-    // waiting for the result
-    const result = await response.json();
-    // the result is the new updated listings array
-    this.listing = result;
-    return this.listing;
-  }
-
-  /***** Update listing *****/
-  async updateListing(
-    id,
-    title,
-    price,
-    description,
-    expirationDate,
-    location,
-    image
-  ) {
+  async updateListing(id, title, price, expirationDate, description, location, image) {
     const listingToUpdate = {
       // declaring a new js object with the form values
       id,
       title,
       price,
-      description,
       expirationDate,
+      description,
       location,
-      image,
+      image
     };
     // put listing to php service using fetch(...)
     const response = await fetch(this.baseUrl + "?action=updateListing", {
@@ -138,14 +136,6 @@ class Service {
     // the result is the new updated listings array
     this.listings = result;
     return this.listings;
-  }
-
-  /***** User *****/
-  async getUser() {
-    const url = `${this.baseUrl}?action=getUser&userId=${userId}`;
-    const response = await fetch(url);
-    const user = await response.json();
-    return user;
   }
 }
 
