711a8d496635b24261bd2262b81e0aae7f296c0e
sandranielsen <eaasmmn@students.eaaa.com>
2021-12-13 14:32:02 +0100

    adding edit and delete buttons

--- dist/index.d56a3cb1.js
+++ dist/index.d56a3cb1.js
@@ -2051,6 +2051,7 @@ class MyListingsPage {
         this.backImg = require("../img/back.svg");
         this.favouritesImg = require("../img/favourites.svg");
         this.render();
+        this.init();
     }
     render() {
         document.querySelector("#root").insertAdjacentHTML("beforeend", /*html*/ `
@@ -2064,9 +2065,9 @@ class MyListingsPage {
           <h2>My Listings</h2>
         </header>
 
-          <!-- Product listing container -->
-       <div class="product-listing-container">
-       <div class="product-listing-image">
+        <!--- Product listing container --->
+       <div class="product-listing-container"></div>
+       <!--- <div class="product-listing-image">
         <button class="favourite-button">
           <img src="${this.favouritesImg}">
         </button>        
@@ -2078,66 +2079,63 @@ class MyListingsPage {
             <button>Edit</button>
             <button style="margin-left: 10px;">Delete</button>
           </div>
-       </div>
-    </div>
+       </div> --->
+
     </section>
     `);
         /* Button router(s) */ document.querySelector("#back-btn8").addEventListener("click", ()=>{
             _routerJsDefault.default.navigateTo("/profile");
         });
     }
-    /*
-  appendListingData() {
-    let htmlTemplate = /*html*/ /* `
-            <article class="selectedListing">
-            <img src="${service.fileUploadUrl}/files/medium/${
-      this.selectedListing.image || "placeholder.jpg"
-    }">
-                <h3>${this.selectedListing.Title}</h3>
-                <button type="button" class="update">Edit</button>
-                <button type="button" class="delete">Delete</button>
-            </article>
-        `;
-
-    document.querySelector(`#${this.id} .selectedListing`).innerHTML =
-      htmlTemplate;
-
-    this.attachEvents();
-  }
-
-  /* Attaching events to DOM elements */ /*
-  attachEvents() {
-    document.querySelectorAll(`#${this.id} [data-listing-id]`)
-      .forEach((element) => {
-        // adds .onclick for every listing calling router.navigateTo(...) with the id of the listing
-        element.onclick = () => {
-          const listingId = element.getAttribute("data-listing-id");
-          router.navigateTo(`/product-page/${listingId}`);
-        };
-      });
-
-    document.querySelector(`#${this.id} .update`).onclick = () =>
-      router.navigateTo(`/update/${this.selectedListing.id}`);
-
-    document.querySelector(`#${this.id} .delete`).onclick = () =>
-      this.showDeleteDialog();
-  }
-
-  async showDeleteDialog() {
-    const deleteListing = confirm("Do you want to delete listing?");
-
-    if (deleteListing) {
-      const listings = await service.deleteListing(this.selectedListing.id);
-      router.navigateTo("/", {
-        listings : listings,
-      });
+    async init() {
+        const listings = await _serviceJsDefault.default.getListings();
+        this.appendListings(listings);
     }
-  }
+    /* Appends the listings to the specified container defined in the render() */ appendListings(listings) {
+        let htmlTemplate = "";
+        for (const listing of listings)htmlTemplate += /*html*/ `
+      <article data-listing-id="${listing.id}">
+      <div style="background-image: url('${listing.listing_img}');" class="product-listing-image">
+        <button class="favourite-button">
+          <img src="${this.favouritesImg}" class="favourite_img">
+        </button>        
+      </div>
 
-  */ beforeShow(props) {
-        console.log(props);
-    // this.selectedListing = await service.getListing(props.id);
-    //this.appendListingData();
+      <!-- Product information container -->
+      <div class="product-listing-info-container">
+        <h3>${listing.listing_title}</h3>
+        <button type="button" class="update">Edit</button>
+        <button type="button" class="delete">Delete</button>
+      </div>
+    </article>
+    `;
+        document.querySelector(`#${this.id} .product-listings-container`).innerHTML = htmlTemplate;
+        this.attachEvents();
+    }
+    /* Attaching events to DOM elements */ attachEvents() {
+        document.querySelectorAll(`#${this.id} [data-listing-id]`).forEach((element)=>{
+            // adds .onclick for every listing calling router.navigateTo(...) with the id of the listing
+            element.onclick = ()=>{
+                const listingId = element.getAttribute("data-listing-id");
+                _routerJsDefault.default.navigateTo(`/product-page/${listingId}`);
+            };
+        });
+        /* Update */ document.querySelector(`#${this.id} .update`).onclick = ()=>_routerJsDefault.default.navigateTo(`/update/${this.selectedListing.id}`)
+        ;
+        /* Delete */ document.querySelector(`#${this.id} .delete`).onclick = ()=>this.showDeleteDialog()
+        ;
+    }
+    /* Delete dialog box */ async showDeleteDialog() {
+        const deleteListing = confirm("Do you want to delete listing?");
+        if (deleteListing) {
+            const listings = await _serviceJsDefault.default.deleteListing(this.selectedListing.id);
+            _routerJsDefault.default.navigateTo("/", {
+                listings: listings
+            });
+        }
+    }
+    async beforeShow(props) {
+        if (props.listings) this.appendlistings(props.listings);
     }
 }
 exports.default = MyListingsPage;
--- src/pages/my-listings.js
+++ src/pages/my-listings.js
@@ -8,6 +8,7 @@ export default class MyListingsPage {
     this.backImg = require("../img/back.svg");
     this.favouritesImg = require("../img/favourites.svg");
     this.render();
+    this.init();
   }
 
   render() {
@@ -24,9 +25,9 @@ export default class MyListingsPage {
           <h2>My Listings</h2>
         </header>
 
-          <!-- Product listing container -->
-       <div class="product-listing-container">
-       <div class="product-listing-image">
+        <!--- Product listing container --->
+       <div class="product-listing-container"></div>
+       <!--- <div class="product-listing-image">
         <button class="favourite-button">
           <img src="${this.favouritesImg}">
         </button>        
@@ -38,8 +39,8 @@ export default class MyListingsPage {
             <button>Edit</button>
             <button style="margin-left: 10px;">Delete</button>
           </div>
-       </div>
-    </div>
+       </div> --->
+
     </section>
     `
     );
@@ -50,28 +51,42 @@ export default class MyListingsPage {
     });
   }
 
-  /*
-  appendListingData() {
-    let htmlTemplate = /*html*/ /* `
-            <article class="selectedListing">
-            <img src="${service.fileUploadUrl}/files/medium/${
-      this.selectedListing.image || "placeholder.jpg"
-    }">
-                <h3>${this.selectedListing.Title}</h3>
-                <button type="button" class="update">Edit</button>
-                <button type="button" class="delete">Delete</button>
-            </article>
-        `;
+  async init() {
+    const listings = await service.getListings();
+    this.appendListings(listings);
+  }
 
-    document.querySelector(`#${this.id} .selectedListing`).innerHTML =
-      htmlTemplate;
+  /* Appends the listings to the specified container defined in the render() */
+  appendListings(listings) {
+    let htmlTemplate = "";
+    for (const listing of listings) {
+      htmlTemplate += /*html*/ `
+      <article data-listing-id="${listing.id}">
+      <div style="background-image: url('${listing.listing_img}');" class="product-listing-image">
+        <button class="favourite-button">
+          <img src="${this.favouritesImg}" class="favourite_img">
+        </button>        
+      </div>
 
+      <!-- Product information container -->
+      <div class="product-listing-info-container">
+        <h3>${listing.listing_title}</h3>
+        <button type="button" class="update">Edit</button>
+        <button type="button" class="delete">Delete</button>
+      </div>
+    </article>
+    `;
+    }
+
+    document.querySelector(
+      `#${this.id} .product-listings-container`).innerHTML = htmlTemplate;
     this.attachEvents();
   }
 
-  /* Attaching events to DOM elements */ /*
+  /* Attaching events to DOM elements */
   attachEvents() {
-    document.querySelectorAll(`#${this.id} [data-listing-id]`)
+    document
+      .querySelectorAll(`#${this.id} [data-listing-id]`)
       .forEach((element) => {
         // adds .onclick for every listing calling router.navigateTo(...) with the id of the listing
         element.onclick = () => {
@@ -79,30 +94,30 @@ export default class MyListingsPage {
           router.navigateTo(`/product-page/${listingId}`);
         };
       });
+    
 
+    /* Update */
     document.querySelector(`#${this.id} .update`).onclick = () =>
       router.navigateTo(`/update/${this.selectedListing.id}`);
 
+    /* Delete */
     document.querySelector(`#${this.id} .delete`).onclick = () =>
       this.showDeleteDialog();
   }
 
+  /* Delete dialog box */
   async showDeleteDialog() {
     const deleteListing = confirm("Do you want to delete listing?");
 
     if (deleteListing) {
       const listings = await service.deleteListing(this.selectedListing.id);
-      router.navigateTo("/", {
-        listings : listings,
-      });
+      router.navigateTo("/", {listings: listings,});
     }
   }
 
-  */
-
-  beforeShow(props) {
-    console.log(props);
-    // this.selectedListing = await service.getListing(props.id);
-    //this.appendListingData();
+  async beforeShow(props) {
+    if (props.listings) {
+      this.appendlistings(props.listings);
+    }
   }
 }
